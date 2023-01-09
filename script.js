92f3ba3f81e16a82ecefa0eb5a0243699af56236
JOse Eloy <joseeloymontenegro@gmail.com>
2023-01-09 11:19:15 -0600

    Primera Subida del proyecto

---
+++ script.js
@@ -0,0 +1,17 @@
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const idp = document.querySelector('#idp')
+const input = document.querySelector('input');
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    idp,
+    input,
+});
+
+console.log(h1);
+
+console.log(input.value);
